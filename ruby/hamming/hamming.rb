4186c480973afc65e808b3e13ca4b9bcfd637856
Giniouxe <aurelie.ginioux@gmail.com>
2015-01-08 11:44:03 +0100

    Refactoring

--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -1,10 +1,13 @@
 class Hamming
+  #Calculate Hamming distance
 
   def self.compute(origin, mutation)
-    pairs = origin.split("").zip(mutation.split(""))
+    #Counts hamming distance between to DNA strings
+
+    pairs = origin.split('').zip(mutation.split(''))
     count = 0
 
-    pairs.each {|i| count += 1 if i[0] != i[1]}
+    pairs.each { |i| count += 1 if i[0] != i[1] }
     count
   end
 
