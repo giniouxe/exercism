1e90fa1465623c9668bd4e35ff9ba2c1f49ccd62
Giniouxe <aurelie.ginioux@gmail.com>
2015-01-10 18:24:59 +0100

    Refactor last condition with too much repetitions

--- ruby/raindrops/raindrops.rb
+++ ruby/raindrops/raindrops.rb
@@ -7,7 +7,8 @@ class Raindrops
     result << 'Pling' if number % 3 == 0
     result << 'Plang' if number % 5 == 0
     result << 'Plong' if number % 7 == 0
-    result << number.to_s if number % 3 != 0 && number % 5 != 0 && number % 7 != 0
+    result << number.to_s if result == ''
     result
   end
+
 end
