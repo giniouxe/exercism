ee985b4ae1493768f8511df06d8a99224d596bb9
Giniouxe <aurelie.ginioux@gmail.com>
2015-03-10 12:03:46 +0100

    Refactor

--- ruby/series/series.rb
+++ ruby/series/series.rb
@@ -1,15 +1,14 @@
 class Series
 	def initialize(chain)
-		@chain = chain
+		@digits = chain.chars.map! { |n| n.to_i }
 	end
 
 	def slices(limit)
-		raise ArgumentError if limit > @chain.length
-		digits = @chain.split('').map! { |n| n.to_i }
+		raise ArgumentError if limit > @digits.length
 		start = 0
 		series = []
-		until (limit + start) == (digits.length + 1) do
-			series <<  digits.slice(start...limit+start)
+		until (limit + start) == (@digits.length + 1) do
+			series <<  @digits.slice(start...limit+start)
 			start += 1
 		end
 		series
