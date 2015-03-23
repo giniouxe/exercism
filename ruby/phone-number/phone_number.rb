7b61ae18610a7608a56a1573b3105e2a4057810b
Giniouxe <aurelie.ginioux@gmail.com>
2015-03-23 12:19:03 +0100

    Refactor - Set phone parts in constructors and avoid number recalculation

--- ruby/phone-number/phone_number.rb
+++ ruby/phone-number/phone_number.rb
@@ -2,10 +2,12 @@
 class PhoneNumber
   DEFAULT = '0000000000'
 
+  # Clean given phone number
   def initialize(phone)
     @phone = phone.tr('. ()-', '')
   end
 
+  # Filter wrong phone numbers and clean long stance calls
   def number
     if @phone.length == 11 && @phone[0] == '1'
       @phone[1..@phone.length]
@@ -18,11 +20,23 @@ class PhoneNumber
     end
   end
 
+  # Set area code
   def area_code
     number[0..2]
   end
 
+  # Set 3 first numbers
+  def prefix
+    number[3..5]
+  end
+
+  # Set 4 last numbers
+  def suffix
+    number[6..9]
+  end
+
+  # Return well formatted number
   def to_s
-    "(#{number[0..2]}) #{number[3..5]}-#{number[6..9]}"
+    '(' + area_code + ') ' + prefix + '-' + suffix
   end
 end
