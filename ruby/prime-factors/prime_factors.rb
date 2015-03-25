53ab8c3dab6f87837e3678d0b9aac22f14352c7c
Giniouxe <aurelie.ginioux@gmail.com>
2015-03-25 12:27:29 +0100

    Refactor

--- ruby/prime-factors/prime_factors.rb
+++ ruby/prime-factors/prime_factors.rb
@@ -1,16 +1,14 @@
 require 'prime'
 
+# Computes the prime factors of a given natural number.
 class PrimeFactors
   def self.for(number)
-    primes = (2..number).select { |n| n.prime? }
+    primes = (2..number).select(&:prime?)
 
     [].tap do |prime_factors|
       while number > 1
         primes.each do |prime|
-          if number % prime == 0
-            prime_factors << prime
-            number /= prime
-          end
+          prime_factors << prime && number /= prime if number % prime == 0
         end
       end
     end
