e94d539ee41792e402a2b7707dfffc061c34d86a
Giniouxe <aurelie.ginioux@gmail.com>
2015-03-25 16:06:06 +0100

    Proper yield

--- ruby/accumulate/array.rb
+++ ruby/accumulate/array.rb
@@ -2,7 +2,7 @@ class Array
   def accumulate(&operation)
     [].tap do |new_collection|
       each do |element|
-        new_collection << operation.yield(element)
+        new_collection << yield(element)
       end
     end
   end
