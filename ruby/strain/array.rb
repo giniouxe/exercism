cc34eba8167a429da0a0e0355ac7eb277e1eec3b
Giniouxe <aurelie.ginioux@gmail.com>
2015-03-27 17:59:17 +0100

    Reimplement keep_if and reject methods instead of using it

--- ruby/strain/array.rb
+++ ruby/strain/array.rb
@@ -1,9 +1,21 @@
 class Array
-	def keep
-		self.keep_if { |item| yield(item) }
-	end
+  def keep
+    return enum_for(:keep) unless block_given?
 
-	def discard
-		self.reject { |item| yield(item) }
-	end
+    [].tap do |collection|
+      each do |element|
+        collection << element if yield(element)
+      end
+    end
+  end
+
+  def discard
+    return enum_for(:discard) unless block_given?
+
+    [].tap do |collection|
+      each do |element|
+        collection << element if !yield(element)
+      end
+    end
+  end
 end
