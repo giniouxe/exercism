8b44eda295f58d838321b3e5cc78f3b0f6d4c6b6
Giniouxe <aurelie.ginioux@gmail.com>
2015-03-27 16:37:31 +0100

    Rename variables to be more consistent

--- ruby/etl/etl.rb
+++ ruby/etl/etl.rb
@@ -1,8 +1,8 @@
 class ETL
   def self.transform(old)
     nice = []
-    old.invert.each_pair do |k, v|
-      k.each { |letter| nice << [letter.downcase, v] }
+    old.invert.each_pair do |letters, score|
+      letters.each { |letter| nice << [letter.downcase, score] }
     end
     nice.to_h
   end
