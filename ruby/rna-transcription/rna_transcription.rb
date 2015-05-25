43ea8cf21b643614748278ea57593f70b057881c
Giniouxe <aurelie.ginioux@gmail.com>
2015-05-25 21:53:13 +0200

    Use fail to raise ArgumentError and don't modify the caller

--- ruby/rna-transcription/rna_transcription.rb
+++ ruby/rna-transcription/rna_transcription.rb
@@ -1,11 +1,11 @@
 class Complement
   def self.of_dna(strand)
-    raise ArgumentError, "#{strand} is not a DNA strand" if strand.include?('U')
-    strand.tr!('CGTA', 'GCAU')
+    fail ArgumentError, "#{strand} is not a DNA strand" if strand.include?('U')
+    strand.tr('CGTA', 'GCAU')
   end
 
   def self.of_rna(strand)
-		raise ArgumentError, "#{strand} is not a RNA strand" if strand.include?('T')
-    strand.tr!('GCAU', 'CGTA')
+    fail ArgumentError, "#{strand} is not a RNA strand" if strand.include?('T')
+    strand.tr('GCAU', 'CGTA')
   end
 end
