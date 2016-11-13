ce6853a5b71dbb5c8c665bc65488ac4a00ee4b08
Giniouxe <aurelie.ginioux@gmail.com>
2016-11-13 18:24:34 +0100

    Get rid of the awfull alphabet array

--- ruby/pangram/pangram.rb
+++ ruby/pangram/pangram.rb
@@ -7,7 +7,7 @@ end
 
 # Used to determine if a sentence is a pangram
 class Pangram
-  ALPHABET = %w(a b c d e f g h i j k l m n o p q r s t u v w x y z)
+  ALPHABET = ('a'..'z').to_a
 
   attr_reader :sentence
 
