6b3e9a29f1049db2b5f813110b23cfeed94bfc0e
Giniouxe <aurelie.ginioux@gmail.com>
2018-08-12 18:57:06 +0200

    [Elixir] Roman numeral - Second iteration

--- elixir/roman-numerals/roman.exs
+++ elixir/roman-numerals/roman.exs
@@ -2,41 +2,36 @@ defmodule Roman do
   @doc """
   Convert the number to a roman number.
   """
-  @mapping %{
-    1 => "I",
-    4 => "IV",
-    5 => "V",
-    9 => "IX",
-    10 => "X",
-    40 => "XL",
-    50 => "L",
-    90 => "XC",
-    100 => "C",
-    400 => "CD",
-    500 => "D",
-    900 => "CM",
-    1000 => "M"
-  }
+  @mapping [
+    {1000, "M"},
+    {900, "CM"},
+    {500, "D"},
+    {400, "CD"},
+    {100, "C"},
+    {90, "XC"},
+    {50, "L"},
+    {40, "XL"},
+    {10, "X"},
+    {9, "IX"},
+    {5, "V"},
+    {4, "IV"},
+    {1, "I"}
+  ]
   @spec numerals(pos_integer) :: String.t()
   def numerals(number) do
-    collect_divisors([], number)
-    |> Enum.map(fn(x) -> Map.get(@mapping, x) end)
+    collect_roman_numerals([], number)
     |> Enum.join("")
   end
 
-  defp collect_divisors(list, 0) do
+  defp collect_roman_numerals(list, 0) do
     Enum.reverse(list)
   end
-  defp collect_divisors(list, number) do
-    collect_divisors(
-      [highest_divisor_for(number) | list],
-      number - highest_divisor_for(number)
-    )
+  defp collect_roman_numerals(list, number) do
+    {divisor, roman_numeral} = highest_divisor_for(number)
+    collect_roman_numerals([roman_numeral | list], number - divisor)
   end
 
   defp highest_divisor_for(number) do
-    Map.keys(@mapping)
-    |> Enum.reverse()
-    |> Enum.find(fn(divisor) -> Integer.floor_div(number, divisor) > 0 end)
+    Enum.find(@mapping, fn(x) -> Integer.floor_div(number, elem(x, 0)) > 0 end)
   end
 end
