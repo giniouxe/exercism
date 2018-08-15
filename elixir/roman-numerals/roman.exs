913e965320ed14574c563151915a032988baf77a
Giniouxe <aurelie.ginioux@gmail.com>
2018-08-15 19:21:48 +0200

    [Elixir] Roman numeral - V2 - Third iteration

--- elixir/roman-numerals/roman.exs
+++ elixir/roman-numerals/roman.exs
@@ -1,49 +1,21 @@
 defmodule Roman do
   @doc """
-  Convert the number to a roman number.
+  Convert the n to a roman n.
   """
   @spec numerals(pos_integer) :: String.t()
-  def numerals(number) do
-    collect_numerals("", number)
-  end
+  def numerals(n), do: do_numerals("", n)
 
-  defp collect_numerals(string, number) when number >= 1000 do
-    String.replace_suffix(string, "", "M") |> collect_numerals(number - 1000)
-  end
-  defp collect_numerals(string, number) when number >= 900 do
-    String.replace_suffix(string, "", "CM") |> collect_numerals(number - 900)
-  end
-  defp collect_numerals(string, number) when number >= 500 do
-    String.replace_suffix(string, "", "D") |> collect_numerals(number - 500)
-  end
-  defp collect_numerals(string, number) when number >= 400 do
-    String.replace_suffix(string, "", "CD") |> collect_numerals(number - 400)
-  end
-  defp collect_numerals(string, number) when number >= 100 do
-    String.replace_suffix(string, "", "C") |> collect_numerals(number - 100)
-  end
-  defp collect_numerals(string, number) when number >= 90 do
-    String.replace_suffix(string, "", "XC") |> collect_numerals(number - 90)
-  end
-  defp collect_numerals(string, number) when number >= 50 do
-    String.replace_suffix(string, "", "L") |> collect_numerals(number - 50)
-  end
-  defp collect_numerals(string, number) when number >= 40 do
-    String.replace_suffix(string, "", "XL") |> collect_numerals(number - 40)
-  end
-  defp collect_numerals(string, number) when number >= 10 do
-    String.replace_suffix(string, "", "X") |> collect_numerals(number - 10)
-  end
-  defp collect_numerals(string, number) when number >= 9 do
-    String.replace_suffix(string, "", "IX") |> collect_numerals(number - 9)
-  end
-  defp collect_numerals(string, number) when number >= 5 do
-    String.replace_suffix(string, "", "V") |> collect_numerals(number - 5)
-  end
-  defp collect_numerals(string, number) when number >= 4 do
-    String.replace_suffix(string, "", "IV") |> collect_numerals(number - 4)
-  end
-  defp collect_numerals(string, number) when number >= 0 do
-    String.duplicate("I", number) |> String.replace_prefix("", string)
-  end
+  defp do_numerals(string, n) when n >= 1000, do: string <> "M" |> do_numerals(n - 1000)
+  defp do_numerals(string, n) when n >= 900, do: string <> "CM" |> do_numerals(n - 900)
+  defp do_numerals(string, n) when n >= 500, do: string <> "D" |> do_numerals(n - 500)
+  defp do_numerals(string, n) when n >= 400, do: string <> "CD" |> do_numerals(n - 400)
+  defp do_numerals(string, n) when n >= 100, do: string <> "C" |> do_numerals(n - 100)
+  defp do_numerals(string, n) when n >= 90, do: string <> "XC" |> do_numerals(n - 90)
+  defp do_numerals(string, n) when n >= 50, do: string <> "L" |> do_numerals(n - 50)
+  defp do_numerals(string, n) when n >= 40, do: string <> "XL" |> do_numerals(n - 40)
+  defp do_numerals(string, n) when n >= 10, do: string <> "X" |> do_numerals(n - 10)
+  defp do_numerals(string, n) when n >= 9, do: string <> "IX" |> do_numerals(n - 9)
+  defp do_numerals(string, n) when n >= 5, do: string <> "V" |> do_numerals(n - 5)
+  defp do_numerals(string, n) when n >= 4, do: string <> "IV" |> do_numerals(n - 4)
+  defp do_numerals(string, n) when n >= 0, do: string <> String.duplicate("I", n)
 end
