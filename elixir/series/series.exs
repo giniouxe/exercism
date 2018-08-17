1455772a27d7e892cbcc9747e67b7a4a8a34cd12
Giniouxe <aurelie.ginioux@gmail.com>
2018-08-17 20:14:25 +0200

    [Elixir] Series - First iteration

---
+++ elixir/series/series.exs
@@ -0,0 +1,29 @@
+defmodule StringSeries do
+  @doc """
+  Given a string `s` and a positive integer `size`, return all substrings
+  of that size. If `size` is greater than the length of `s`, or less than 1,
+  return an empty list.
+  """
+  @spec slices(s :: String.t(), size :: integer) :: list(String.t())
+  def slices(s, size) do
+    cond do
+      size > String.length(s) || size <= 0 -> []
+      size == 1 -> String.split(s, "", trim: true)
+      true -> String.split(s, "", trim: true) |> do_slices([], size)
+    end
+  end
+
+  defp do_slices([], slices, _) do
+    Enum.reverse(slices)
+  end
+  defp do_slices([head | tail], slices, size) when length(tail) < size do
+    do_slices([], [do_slice(head, tail, size) | slices], size)
+  end
+  defp do_slices([head | tail], slices, size) do
+    do_slices(tail, [do_slice(head, tail, size) | slices], size)
+  end
+
+  defp do_slice(string, list, size) do
+    Enum.reduce((0..size - 2), string, fn(n, acc) -> "#{acc}#{Enum.at(list, n)}" end)
+  end
+end
