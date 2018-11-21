a35141fb25f9462038cae6985473a20e43399d34
Giniouxe <aurelie.ginioux@gmail.com>
2018-11-21 18:32:52 +0100

    [Elixir] Series - Second iteration

--- elixir/series/series.exs
+++ elixir/series/series.exs
@@ -5,10 +5,15 @@ defmodule StringSeries do
   return an empty list.
   """
   @spec slices(s :: String.t(), size :: integer) :: list(String.t())
+  def slices(_, size) when size <= 0 do
+    []
+  end
+  def slices(s, size) when size == 1 do
+    String.split(s, "", trim: true)
+  end
   def slices(s, size) do
     cond do
-      size > String.length(s) || size <= 0 -> []
-      size == 1 -> String.split(s, "", trim: true)
+      size > String.length(s) -> []
       true -> String.split(s, "", trim: true) |> do_slices([], size)
     end
   end
