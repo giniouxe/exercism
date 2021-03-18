893f9408fb41cb33662d420ab1ddc633214ffc19
giniouxe <aurelie.ginioux@gmail.com>
2021-03-18 17:13:43 +0100

    [Elixir] Sublist - refacto

--- elixir/sublist/sublist.exs
+++ elixir/sublist/sublist.exs
@@ -3,35 +3,28 @@ defmodule Sublist do
   Returns whether the first list is a sublist or a superlist of the second list
   and if not whether it is equal or unequal to the second list.
   """
-  def compare([], []), do: :equal
-  def compare([], _b), do: :sublist
-  def compare(_a, []), do: :superlist
-
   def compare(a, b) when a == b, do: :equal
-  def compare(a, b) when length(a) >= length(b), do: find_superlist(a, b)
-  def compare(a, b), do: find_sublist(a, b)
+  def compare(_a, []), do: :superlist
+  def compare([], _b), do: :sublist
 
-  defp find_superlist(a, b) do
-    case find_sublist(b, a) do
-      :sublist -> :superlist
-      _ -> :unequal
-    end
+  def compare(a, b) when length(a) >= length(b) do
+    if is_sublist?(b, a), do: :superlist, else: :unequal
   end
 
-  defp find_sublist(_a, []), do: :unequal
+  def compare(a, b) do
+    if is_sublist?(a, b), do: :sublist, else: :unequal
+  end
 
-  defp find_sublist(a, b) when a == b, do: :sublist
-
-  defp find_sublist([a_head | _a_tail] = a, b) do
+  defp is_sublist?([a_head | _a_tail] = a, b) do
     case Enum.find_index(b, fn x -> x == a_head end) do
       nil ->
-        :unequal
+        false
 
       index ->
         if remainder(b, index) |> List.starts_with?(a) do
-          :sublist
+          true
         else
-          find_sublist(a, remainder(b, index + 1))
+          is_sublist?(a, remainder(b, index + 1))
         end
     end
   end
